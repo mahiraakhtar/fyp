a343d311f206ae6b469a91256e086bd4d016a1d4
Mahira Akhtar <mahiravidha@gmail.com>
2018-05-07 15:59:03 +0500

    nlp
    nlp

--- nlp/views.py
+++ nlp/views.py
@@ -11,6 +11,8 @@ from nltk.classify.scikitlearn import  SklearnClassifier
 import string
 from string import punctuation
 from .models import umls
+from nltk.tag import StanfordNERTagger
+from nltk.tokenize import word_tokenize
 # Create your views here.
  
 def index(request):
@@ -49,4 +51,72 @@ def insertdatabase(self):
 def createumls(dis,c):
 	d=umls(Diseasename=dis, Code=c)
 	d.save()
+
+
+stop_words= set(stopwords.words("english"))
+ps=PorterStemmer()
+lemmatizer =WordNetLemmatizer()
+sentence_re = r'(?:(?:[A-Z])(?:.[A-Z])+.?)|(?:\w+(?:-\w+)*)|(?:\$?\d+(?:.\d+)?%?)|(?:...|)(?:[][.,;"\'?():-_`])'
+
+def info_ext(text):
+# 	words=(word_tokenize(text))
+	# 
+
+	# for w in filtered_text:
+	# 	stemmed.append()
+
+	# lemmatized=[]
+
+	grammar = r"""
+	NBAR:
+		{<NN.*|JJ>*<NN.*>}  # Nouns and Adjectives, terminated with Nouns
+        
+	NP:
+		{<NBAR>}
+		{<NBAR><IN><NBAR>}  # Above, connected with in/of/etc...
+	"""
+
+	toks = nltk.regexp_tokenize(text, sentence_re)
+	filtered_toks=[w for w in tagged if not w in stop_words]
+	tagged = nltk.tag.pos_tag(filtered_toks)
+
+		
+	# chunkgram=r"""Chunk: {<JJ>+<JJ.>*<NN.>+<NN>?} """
+	chunkParser=nltk.RegexpParser(grammar)
+	chunkedtree=chunkParser.parse(tagged)
+
+	terms = get_terms(chunkedtree)
+
+	for term in terms:
+		for word in term:
+			print (word),
+		print
+
+	
+def leaves(tree):
+	# """Finds NP (nounphrase) leaf nodes of a chunk tree."""
+    for subtree in tree.subtrees(filter = lambda t: t.label()=='NP'):
+    	yield subtree.leaves()
+
+def normalise(word):
+#     """Normalises words to lowercase and stems and lemmatizes it."""
+	word = word.lower()
+    # word = stemmer.stem_word(word) #if we consider stemmer then results comes with stemmed word, but in this case word will not match with comment
+		# ps.stem(word)
+	word = lemmatizer.lemmatize(word)
+	return word
+
+def acceptable_word(word):
+    # """Checks conditions for acceptable word: length
+	accepted = bool(2 <= len(word) <= 40 and word.lower() )
+	return accepted
+
+
+def get_terms(tree):
+	for leaf in leaves(tree):
+		term = [ normalise(w) for w,t in leaf ]
+		# if acceptable_word(w) ]
+		yield term
+
+
 

5021ef0ea963e1bfa0ec62860f0ae7a2934368d9
Mahira Akhtar <mahiravidha@gmail.com>
2018-05-24 14:23:32 +0500

    rules

--- patientdetails/urls.py
+++ patientdetails/urls.py
@@ -11,10 +11,20 @@ urlpatterns = [
     path('addpatient/', views.patientcreate.as_view(), name='patientcreate'),
     path('patientupdate/<pk>/', views.patientupdate.as_view(), name='patientupdate'),
     path('deletepatient/<pk>/', views.patientdelete.as_view(), name='patientdelete'),
+    path('<patient_id>/deletetest/<testresult_id>/', views.testdelete, name='testdelete'),
     path('addtest/<patient_id>/', views.testcreate, name='testcreate'),
+
+    path('rules/', views.rulesindex.as_view(), name='rulesindex'),
+    path('rules/<pk>/', views.rulesdetail.as_view(), name='rulesdetail'),
+    path('addrule/', views.rulescreate.as_view(), name='rulescreate'),
+    path('rulesupdate/<pk>/', views.rulesupdate.as_view(), name='rulesupdate'),
+    path('rulesdelete/<pk>/', views.rulesdelete.as_view(), name='rulesdelete'),
+    path('<rules_id>/deleteparam/<param_id>/', views.rulesdelete, name='paramdelete'),
+    path('addparam/<rules_id>/', views.paramcreate, name='paramcreate'),
+
     path('inferform/', views.inferform, name='inferform'),
     path('infer/', views.infer, name='infer'),
-    path('<patient_id>/deletetest/<testresult_id>/', views.testdelete, name='testdelete'),
+    
 
 	path('insertsymplist/', views.symplist, name='insertsymplist'),
 	path('addtest/', views.addtest.as_view(), name='addtest'),
--- user/migrations/0001_initial.py
+++ user/migrations/0001_initial.py
@@ -4,26 +4,26 @@ from django.db import models, migrations
 from django.contrib.auth.models import Group, Permission
 
 
+
 def add_group_permissions(apps, schema_editor):
     #non staff
     group, created = Group.objects.get_or_create(name='non_staff')
     if created:
-        #group.permissions.add(can_read_campaign)
-        logger.info('non_staff Group created')
+
         group.save()
 
     #assistant
     group, created = Group.objects.get_or_create(name='assistant')
     if created:
         #group.permissions.add(can_edit_users)
-        logger.info('assistant Group created')
+
         group.save()
 
     #doctor
     group, created = Group.objects.get_or_create(name='doctor')
     if created:
-        group.permissions.add('pateintdetails.view_patient', 'pateintdetails.add_patient', 'pateintdetails.change_patient', 'pateintdetails.delete_patient')
-        logger.info('doctor Group created')
+        # group.permissions.add('pateintdetails.view_patient', 'pateintdetails.add_patient', 'pateintdetails.change_patient', 'pateintdetails.delete_patient')
+  
         group.save()
 
 
